8ac73625bc7d1b57242252a2c616854ff8491d50
KunAgueroo <103275401+KunAgueroo@users.noreply.github.com>
2022-04-08 18:04:26 -0400

    Add files via upload

---
+++ Compare GPA/include/gpa.h
@@ -0,0 +1,13 @@
+#ifndef GPA_H
+#define GPA_H
+#include <stdio.h>
+#include <stdlib.h>
+
+struct person {
+  char name[20];
+  int age;
+  double GPA;
+};
+void find_the_person(struct person persons[], int len);
+
+#endif
---
+++ Compare GPA/src/gpa.c
@@ -0,0 +1,39 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "gpa.h"
+#include <unistd.h>
+// return: struct person
+// param: (struct person p1, struct person p2)
+// TODO: create a function that returns the person who has a higher GPA.
+// TODO: if GPAs are equal, then return the first person (the first parameter).
+// TODO: this function can only be called in the file where it is declared.
+static struct person compareTo(struct person p1, struct person p2) {
+if (p1.GPA > p2.GPA) 
+{
+  return p1;
+}
+else if (p1.GPA == p2.GPA)
+{
+  return p1;
+}
+else
+{
+  return p2;
+}
+}
+
+
+// return: void
+// param: (struct person persons[], int len)
+// TODO: create a function that returns the person who has the highest GPA.
+// TODO: update the value of the_person in the main.c file to this person.
+// TODO: this function must be callable from other modules.
+void find_the_person(struct person persons[], int len) {
+extern struct person the_person; 
+int i;
+the_person = persons[0];
+for (i = 1; i < len; i++)
+{
+  the_person = compareTo(the_person, persons[i]);
+}
+}
